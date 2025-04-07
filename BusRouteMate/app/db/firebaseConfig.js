25721061bff91ea8cc900e4d863fb7f43c584ec1
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2025-04-07 20:05:24 +0530

    Added a basic location tracking demo code

--- BusRouteMate/app/db/firebaseConfig.js
+++ BusRouteMate/app/db/firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp,getApp,getApps} from 'firebase/app';
 // import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getAuth,initializeAuth,getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import {getDatabase}from 'firebase/database';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
@@ -10,12 +11,14 @@ const firebaseConfig = {
   projectId: "bus-route-mate",
   storageBucket: "bus-route-mate.firebasestorage.app",
   messagingSenderId: "211407313322",
-  appId: "1:211407313322:web:5fd614cfb87f3f8fa273d3" 
+  appId: "1:211407313322:web:5fd614cfb87f3f8fa273d3", 
+  databaseURL:"https://bus-route-mate-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
+const realtimeDb = getDatabase(app);
 
-export { auth, db };
+export { auth, db, realtimeDb };
